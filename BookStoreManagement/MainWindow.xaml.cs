ccf83d14287a0c24d40f8c752e305d5cf8a91105
Kritsanaphong Tepwerakul <kritsanaphong_t@hotmail.com>
2024-06-17 01:29:12 +0700

    Added Login Page

--- BookStoreManagement/MainWindow.xaml.cs
+++ BookStoreManagement/MainWindow.xaml.cs
@@ -22,5 +22,41 @@ namespace BookStoreManagement
         {
             InitializeComponent();
         }
+
+        private void passwordTxt_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (passwordTxt.Password.Length > 0)
+            {
+                passwordLabel.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                passwordLabel.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void usernameTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (usernameTxt.Text.Length > 0)
+            {
+                usernameLabel.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                usernameLabel.Visibility= Visibility.Visible;
+            }
+        }
+
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            if (usernameTxt.Text == "admin" && passwordTxt.Password == "123")
+            {
+                MessageBox.Show("Login success.", "Message");
+            }
+            else
+            {
+                MessageBox.Show("Username or password is incorrect.", "Message", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
     }
 }
